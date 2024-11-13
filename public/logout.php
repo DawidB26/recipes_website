65dfbd16e825a67641edc1be76f350b3cdcd740c
Dawid <dawidbrzeski06@wp.pl>
2024-11-13 22:15:54 +0100

    naprawienie

--- public/logout.php
+++ public/logout.php
@@ -4,4 +4,5 @@ require_once '../includes/db.php';
 session_start();
 session_unset();
 session_destroy();
-echo json_encode(["status" => "success", "message" => "User logged out successfully."]);
+exit();
+?>
