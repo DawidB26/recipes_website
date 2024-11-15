a2f3717ce6c5369c1812c4f4347379c2e92ee1bc
BogdanGarkovenko <147413199+BogdanGarkovenko@users.noreply.github.com>
2024-11-15 20:59:21 +0100

    Create header.php

---
+++ blocks/header.php
@@ -0,0 +1,14 @@
+<header>
+    <h1>Najpyszniejsze Przepisy</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Strona Główna</a></li>
+                <li><a href="#">Przepisy</a></li>
+                <li><a href="add-recipe.php">Dodaj przepis</a></li>
+                <li><a href="#">Ulubione</a></li>
+                <li><a href="#">Moje Przepisy</a></li>
+                <li><a href="#" id="login-btn">Zaloguj się</a></li>
+                <li><a href="#" id="register-btn">Zarejestruj się</a></li>
+            </ul>
+        </nav>
+</header>
