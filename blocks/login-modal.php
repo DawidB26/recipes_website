89325555c7f646bd575224e47104bbc6936e1337
BogdanGarkovenko <147413199+BogdanGarkovenko@users.noreply.github.com>
2024-11-15 20:59:53 +0100

    Add files via upload

---
+++ blocks/login-modal.php
@@ -0,0 +1,10 @@
+<div id="login-modal" class="modal">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Zaloguj się</h2>
+        <button id="google-login">Zaloguj przez Google</button>
+        <button id="facebook-login">Zaloguj przez Facebook</button>
+        <button id="instagram-login">Zaloguj przez Instagram</button>
+        <button id="x-login">Zaloguj przez X</button>
+    </div>
+</div>
