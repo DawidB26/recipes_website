946b51e2373f5cf5ff2329e80f4ca465a8d9656a
BogdanGarkovenko <147413199+BogdanGarkovenko@users.noreply.github.com>
2024-11-28 21:35:05 +0100

    Add files via upload

--- blocks/header.php
+++ blocks/header.php
@@ -2,14 +2,14 @@
     <h1>Najpyszniejsze Przepisy</h1>
         <nav>
             <ul>
-                <li><a href="index.php">Strona Główna</a></li>
-                <li><a href="search.php">Wyszukiwarka</a></li>
-                <li><a href="add_recipe.php">Dodaj przepis</a></li>
-                <li><a href="public/favorites.php">Ulubione</a></li>
-                <li><a href="public/my_recipes.php">Moje Przepisy</a></li>
+                <li><a href="http://localhost/dashboard/strona_z_przepisami/index.php">Strona Główna</a></li>
+                <li><a href="http://localhost/dashboard/strona_z_przepisami/search.php">Wyszukiwarka</a></li>
+                <li><a href="http://localhost/dashboard/strona_z_przepisami/add_recipe.php">Dodaj przepis</a></li>
+                <li><a href="http://localhost/dashboard/strona_z_przepisami/public/favorites.php">Ulubione</a></li>
+                <li><a href="http://localhost/dashboard/strona_z_przepisami/public/my_recipes.php">Moje Przepisy</a></li>
                 <li><a href="#" id="login-btn">Zaloguj się</a></li>
                 <li><a href="#" id="register-btn">Zarejestruj się</a></li>
-                <li><a href="public/logout.php">Wyloguj</a></li>
+                <li><a href="http://localhost/dashboard/strona_z_przepisami/public/logout.php">Wyloguj</a></li>
             </ul>
         </nav>
 </header>
--- blocks/login-modal.php
+++ blocks/login-modal.php
@@ -2,7 +2,7 @@
     <div class="modal-content">
         <span class="close">&times;</span>
         <h1>Logowanie</h1>
-        <form method="POST" action="http://localhost/public/login.php">
+        <form method="POST" action="../strona_z_przepisami/public/login.php">
             <label for="username">Nazwa użytkownika:</label>
             <input type="text" id="username" name="username" required>
 
@@ -11,7 +11,7 @@
 
             <button type="submit">Zaloguj</button>
         </form>
-        <h2>Zaloguj się przez</h2>
+        <h2>Lub zaloguj się przez</h2>
         <button id="google-login">Google</button>
         <button id="facebook-login">Facebook</button>
     </div>
